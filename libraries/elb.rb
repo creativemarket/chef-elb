06375ec0190d8eda550bc5b632f09af292e729f4
jeremykoerber <jeremy@creativemarket.com>
2015-06-16 22:08:38 -0700

    restructure conditionals

--- libraries/elb.rb
+++ libraries/elb.rb
@@ -1,55 +1,54 @@
 module Cm
 	module Aws
 		module Elb
-			if new_resource.aws_access_key_id.nil? then
-				def elb
-				@@elb ||= Fog::AWS::ELB.new(
-					:use_iam_profile => true,
-					:region => new_resource.region
-				)
-				end
-
-				def ec2
-				@@ec2 ||= Fog::Compute.new(:provider => 'AWS',
-					:use_iam_profile => true,
-					:region => new_resource.region
-				)
-				end
-			else
-				def elb
-				@@elb ||= Fog::AWS::ELB.new(
-					:use_iam_profile => false,
-					:region => new_resource.region,
-					:aws_access_key_id => new_resource.aws_access_key_id,
-					:aws_secret_access_key => new_resource.aws_secret_access_key
-				)
-				end
-
-				def ec2
-				@@ec2 ||= Fog::Compute.new(:provider => 'AWS',
-					:use_iam_profile => false,
-					:region => new_resource.region,
-					:aws_access_key_id => new_resource.aws_access_key_id,
-					:aws_secret_access_key => new_resource.aws_secret_access_key
+			def elb
+				if new_resource.aws_access_key_id.nil?
+					@@elb ||= Fog::AWS::ELB.new(
+						:use_iam_profile => true,
+						:region => new_resource.region
+					)
+				else
+					@@elb ||= Fog::AWS::ELB.new(
+						:use_iam_profile => false,
+						:region => new_resource.region,
+						:aws_access_key_id => new_resource.aws_access_key_id,
+						:aws_secret_access_key => new_resource.aws_secret_access_key
 					)
 				end
 			end
 
-		def load_balancer_by_name(name)
-		elb.describe_load_balancers.body["DescribeLoadBalancersResult"]["LoadBalancerDescriptions"].detect { |lb| lb["LoadBalancerName"] == new_resource.lb_name }
-		end
+			def ec2
+				if new_resource.aws_access_key_id.nil?
+					@@ec2 ||= Fog::Compute.new(:provider => 'AWS',
+						:use_iam_profile => true,
+						:region => new_resource.region
+					)
+				else
+					@@ec2 ||= Fog::Compute.new(:provider => 'AWS',
+						:use_iam_profile => false,
+						:region => new_resource.region,
+						:aws_access_key_id => new_resource.aws_access_key_id,
+						:aws_secret_access_key => new_resource.aws_secret_access_key
+					)
+				end
+			end
 
-		def policies_for_load_balancer(name)
-		elb.describe_load_balancer_policies(name).body["DescribeLoadBalancerPoliciesResult"]["PolicyDescriptions"]
-		end
 
-		def availability_zone_for_instances(instances)
-		ec2.describe_instances('instance-id' => [*instances]).body['reservationSet'].map { |r| r['instancesSet'] }.flatten.map { |i| i['placement']['availabilityZone'] }
-		end
+			def load_balancer_by_name(name)
+				elb.describe_load_balancers.body["DescribeLoadBalancersResult"]["LoadBalancerDescriptions"].detect { |lb| lb["LoadBalancerName"] == new_resource.lb_name }
+			end
 
-		def subnets_for_instances(instances)
-		ec2.describe_instances('instance-id' => [*instances]).body['reservationSet'].map { |r| r['instancesSet'] }.flatten.map { |i| i['networkInterfaces'] }.flatten.map { |n| n['subnetId'] }
+			def policies_for_load_balancer(name)
+				elb.describe_load_balancer_policies(name).body["DescribeLoadBalancerPoliciesResult"]["PolicyDescriptions"]
+			end
+
+			def availability_zone_for_instances(instances)
+				ec2.describe_instances('instance-id' => [*instances]).body['reservationSet'].map { |r| r['instancesSet'] }.flatten.map { |i| i['placement']['availabilityZone'] }
+			end
+
+			def subnets_for_instances(instances)
+				ec2.describe_instances('instance-id' => [*instances]).body['reservationSet'].map { |r| r['instancesSet'] }.flatten.map { |i| i['networkInterfaces'] }.flatten.map { |n| n['subnetId'] }
+			end
 		end
-	end
 	end
 end
